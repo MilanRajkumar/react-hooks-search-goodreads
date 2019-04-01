a329d133c17c56aff02f9e4a635643ce33d76c79
MilanRajkumar <rkmilansingh@gmail.com>
2019-04-02 01:49:20 +0530

    Updates

---
+++ precache-manifest.6a595d9edd228004546c9d384761dc27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbb907c675b55d28bc1a967a64291088",
+    "url": "./static/media/sample_book.dbb907c6.jpg"
+  },
+  {
+    "revision": "8066800e7f4a3e9b131762b68ed110b6",
+    "url": "./static/media/bg.8066800e.jpeg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "58d5ea8569a28a1926c4",
+    "url": "./static/js/main.839d52b9.chunk.js"
+  },
+  {
+    "revision": "96e3f33caf06630656d8",
+    "url": "./static/js/2.6798e57f.chunk.js"
+  },
+  {
+    "revision": "58d5ea8569a28a1926c4",
+    "url": "./static/css/main.10e929bc.chunk.css"
+  },
+  {
+    "revision": "02bfffaf177030294c2e472a62c17141",
+    "url": "./index.html"
+  }
+];
--- src/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export const key = 'gxoRqRP8GeTiwHuouSA';
-export const secret = 'hkInhxiotNT12thFAqQyUDEHCcmcJnimr7KGshGVQ';
